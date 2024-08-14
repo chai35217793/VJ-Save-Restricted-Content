5a326682df5cf6129067fa9b63efca65e143e45f
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-08-14 21:24:40 +0530

    Update save.py

--- TechVJ/save.py
+++ TechVJ/save.py
@@ -1,3 +1,155 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+import asyncio 
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
+import time
+import os
+import threading
+import json
+from config import API_ID, API_HASH
+from database.db import database 
+from TechVJ.strings import strings, HELP_TXT
+
+def get(obj, key, default=None):
+    try:
+        return obj[key]
+    except:
+        return default
+
+
+async def downstatus(client: Client, statusfile, message):
+    while True:
+        if os.path.exists(statusfile):
+            break
+
+        await asyncio.sleep(3)
+      
+    while os.path.exists(statusfile):
+        with open(statusfile, "r") as downread:
+            txt = downread.read()
+        try:
+            await client.edit_message_text(message.chat.id, message.id, f"Downloaded : {txt}")
+            await asyncio.sleep(10)
+        except:
+            await asyncio.sleep(5)
+
+
+# upload status
+async def upstatus(client: Client, statusfile, message):
+    while True:
+        if os.path.exists(statusfile):
+            break
+
+        await asyncio.sleep(3)      
+    while os.path.exists(statusfile):
+        with open(statusfile, "r") as upread:
+            txt = upread.read()
+        try:
+            await client.edit_message_text(message.chat.id, message.id, f"Uploaded : {txt}")
+            await asyncio.sleep(10)
+        except:
+            await asyncio.sleep(5)
+
+
+# progress writer
+def progress(current, total, message, type):
+    with open(f'{message.id}{type}status.txt', "w") as fileup:
+        fileup.write(f"{current * 100 / total:.1f}%")
+
+
+# start command
+@Client.on_message(filters.command(["start"]))
+async def send_start(client: Client, message: Message):
+    buttons = [[
+        InlineKeyboardButton("❣️ Developer", url = "https://t.me/kingvj01")
+    ],[
+        InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/vj_bot_disscussion'),
+        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/vj_botz')
+	]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await client.send_message(message.chat.id, f"<b>👋 Hi {message.from_user.mention}, I am Save Restricted Content Bot, I can send you restricted content by its post link.\n\nFor downloading restricted content /login first.\n\nKnow how to use bot by - /help</b>", reply_markup=reply_markup, reply_to_message_id=message.id)
+    return
+
+
+# help command
+@Client.on_message(filters.command(["help"]))
+async def send_help(client: Client, message: Message):
+    await client.send_message(message.chat.id, f"{HELP_TXT}")
+
+@Client.on_message(filters.text & filters.private)
+async def save(client: Client, message: Message):
+    if "https://t.me/" in message.text:
+        datas = message.text.split("/")
+        temp = datas[-1].replace("?single","").split("-")
+        fromID = int(temp[0].strip())
+        try:
+            toID = int(temp[1].strip())
+        except:
+            toID = fromID
+        for msgid in range(fromID, toID+1):
+            # private
+            if "https://t.me/c/" in message.text:
+                user_data = database.find_one({'chat_id': message.chat.id})
+                if not get(user_data, 'logged_in', False) or user_data['session'] is None:
+                    await client.send_message(message.chat.id, strings['need_login'])
+                    return
+                acc = Client("saverestricted", session_string=user_data['session'], api_hash=API_HASH, api_id=API_ID)
+                await acc.connect()
+                chatid = int("-100" + datas[4])
+                await handle_private(client, acc, message, chatid, msgid)
+    
+            # bot
+            elif "https://t.me/b/" in message.text:
+                user_data = database.find_one({"chat_id": message.chat.id})
+                if not get(user_data, 'logged_in', False) or user_data['session'] is None:
+                    await client.send_message(message.chat.id, strings['need_login'])
+                    return
+                acc = Client("saverestricted", session_string=user_data['session'], api_hash=API_HASH, api_id=API_ID)
+                await acc.connect()
+                username = datas[4]
+                try:
+                    await handle_private(client, acc, message, username, msgid)
+                except Exception as e:
+                    await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
+            
+	        # public
+            else:
+                username = datas[3]
+
+                try:
+                    msg = await client.get_messages(username, msgid)
+                except UsernameNotOccupied: 
+                    await client.send_message(message.chat.id, "The username is not occupied by anyone", reply_to_message_id=message.id)
+                    return
+                try:
+                    await client.copy_message(message.chat.id, msg.chat.id, msg.id, reply_to_message_id=message.id)
+                except:
+                    try:    
+                        user_data = database.find_one({"chat_id": message.chat.id})
+                        if not get(user_data, 'logged_in', False) or user_data['session'] is None:
+                            await client.send_message(message.chat.id, strings['need_login'])
+                            return
+                        acc = Client("saverestricted", session_string=user_data['session'], api_hash=API_HASH, api_id=API_ID)
+                        await acc.connect()
+                        await handle_private(client, acc, message, username, msgid)
+                        
+                    except Exception as e:
+                        await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
+
+            # wait time
+            await asyncio.sleep(3)
+
+
+# handle private
+async def handle_private(client: Client, acc, message: Message, chatid: int, msgid: int):
+    msg: Message = await acc.get_messages(chatid, msgid)
+    msg_type = get_message_type(msg)
+    chat = message.chat.id
     if "Text" == msg_type:
         try:
             await client.send_message(chat, msg.text, entities=msg.entities, reply_to_message_id=message.id)
@@ -140,4 +292,4 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
         return "Text"
     except:
         pass
-                                    
+        
